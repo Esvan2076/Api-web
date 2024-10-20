b8e6091724f11ff6a5db5a48aabc7df1af090a9a
Esteban <estividivi2016@hotmail.com>
2024-10-20 15:00:15 -0600

    Creacion Pro

---
+++ src/main/java/com/webos/esvan/EsvanApplication.java
@@ -0,0 +1,13 @@
+package com.webos.esvan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsvanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsvanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webos/esvan/EsvanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webos.esvan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsvanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
